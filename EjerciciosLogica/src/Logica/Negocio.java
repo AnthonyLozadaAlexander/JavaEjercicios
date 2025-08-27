8ffb7717c4b2736aa238e53ad575f4eb703de2dc
zDefcon <antholozadarequelme@gmail.com>
2025-08-26 21:21:52 -0500

    Agregar archivo Negocio.java para implementar la lógica del sistema de ventas, incluyendo la selección de productos y cálculo de precios.

---
+++ EjerciciosLogica/src/Logica/Negocio.java
@@ -0,0 +1,70 @@
+package Logica;
+
+import java.util.Scanner;
+
+public class Negocio {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int codigo = 0, precioU = 0, cantidadC = 0, cantidadL = 0, cantidadB = 0, cantidadR = 0, pagarC = 0, pagarL = 0,
+                pagarB = 0, pagarR = 0;
+        double totalPagar = 0.0;
+        System.out.println("-------------------------------");
+        System.out.println("Bienvenido al Sistema De Ventas");
+        System.out.println("-------------------------------");
+        System.out
+                .println("Codigo 1: Cuaderno 10$" + "\n" + "Codigo 2: Lapiz $7" + "\n" + "Codigo 3: Borrador $3" + "\n"
+                        + "Codigo 4: Resaltador $1\n");
+        System.out.println("-------------------------------");
+        System.out.print("Ingrese El Codigo Del Producto: ");
+        codigo = input.nextInt();
+        switch (codigo) {
+            case 1:
+                precioU = 10;
+                System.out.print("Ingrese la cantidad de unidades: ");
+                cantidadC = input.nextInt();
+                pagarC += precioU * cantidadC;
+
+                break;
+            case 2:
+                precioU = 7;
+                int cantidadCajas = 0;
+                int cajaLapizPrecio = 65;
+                int opcion = 0;
+                System.out.println("Desea comprar una caja de lapices? (1: Si, 2: No)");
+                opcion = input.nextInt();
+                if (opcion == 1) {
+                    System.out.print("Cuantas Cajas Desea Comprar: ");
+                    cantidadCajas = input.nextInt();
+                    pagarL += cantidadCajas * cajaLapizPrecio;
+                } else if (opcion == 2) {
+                    System.out.print("Ingrese la cantidad de unidades: ");
+                    cantidadL = input.nextInt();
+                    pagarL += precioU * cantidadL;
+                } else {
+                    System.out.println("ERROR: Opcion no valida");
+                }
+
+                break;
+            case 3:
+                precioU = 10;
+                System.out.print("Ingrese la cantidad de unidades: ");
+                cantidadB = input.nextInt();
+                pagarB += precioU * cantidadB;
+
+                break;
+
+            case 4:
+                precioU = 1;
+                System.out.print("Ingrese la cantidad de unidades: ");
+                cantidadR = input.nextInt();
+                pagarR += precioU * cantidadR;
+
+                break;
+            default:
+                System.out.println("ERROR: Codigo No Identificado");
+                break;
+
+        }
+    }
+
+}
