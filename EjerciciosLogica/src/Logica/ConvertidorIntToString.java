fa649bc0ea71a162d580366d857aed192386ffbc
zDefcon <antholozadarequelme@gmail.com>
2025-11-11 00:48:44 -0500

    Modificar la clase ConvertidorIntToString para que los métodos intToString y StringToInt sean estáticos. Se mejora la estructura del código y se asegura que el método StringToInt valide correctamente la entrada antes de convertirla a entero.

--- EjerciciosLogica/src/Logica/ConvertidorIntToString.java
+++ EjerciciosLogica/src/Logica/ConvertidorIntToString.java
@@ -1,7 +1,9 @@
 package Logica;
 
+import java.util.Scanner;
+
 public class ConvertidorIntToString {
-    public String intToString(int num) {
+    public static String intToString(int num) {
         if (num < 0) {
             return "(" + String.valueOf(num) + ")";
         }
@@ -9,12 +11,25 @@ public class ConvertidorIntToString {
         return String.valueOf(num);
     }
 
-    public String StringToInt(String str) {
+    public static int StringToInt(String str) {
+        String regex = "^\\d+(\\.\\d+)?$";
+        if (!str.matches(regex)) {
+            System.out.println("Error: El String Ingresado No Es Un Numero Valido");
+        }
 
+        return Integer.parseInt(str);
     }
 
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("--- Pruebas ConvertidorIntToString ---");
+        System.out.println("Ingrese Numero");
+        System.out.print("-> ");
+        int num = input.nextInt();
+        System.out.println("Numero ingresado: " + num + "\n");
+        System.out.println("Convertir Int a String:");
+        System.out.println("Resultado: " + intToString(num) + "\n");
+        input.nextLine(); // Limpiar buffer
     }
 
 }
