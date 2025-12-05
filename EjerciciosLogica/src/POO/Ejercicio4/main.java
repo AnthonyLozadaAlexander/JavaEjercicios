790785849de89b6c2c0eb442fa8d2a66dd22b153
zDefcon <antholozadarequelme@gmail.com>
2025-12-04 21:30:31 -0500

    Agregar la clase main con el método main para iniciar el sistema de registro de estudiantes, incluyendo la importación de clases necesarias y la creación de una lista de estudiantes.

---
+++ EjerciciosLogica/src/POO/Ejercicio4/main.java
@@ -0,0 +1,21 @@
+package POO.Ejercicio4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Estudiante> estudiantes = new ArrayList<>();
+
+        System.out.println("------------------------------------------------");
+        System.out.println("Bienvenido Al Sistema de Registro de Estudiantes");
+        System.out.println("------------------------------------------------");
+
+        String nombre = "";
+        int edad = 0;
+        String carrera = "";
+
+        System.out.println("");
+    }
+}
