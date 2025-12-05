c7d132a97f0e7990a4b6e3b2e532202167e33e93
zDefcon <antholozadarequelme@gmail.com>
2025-12-04 21:36:12 -0500

    Agregar método para registrar estudiantes en la clase main, mejorando la gestión de la lista de estudiantes.

--- EjerciciosLogica/src/POO/Ejercicio4/main.java
+++ EjerciciosLogica/src/POO/Ejercicio4/main.java
@@ -4,9 +4,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
+
+    public static void agregarEstudiante(ArrayList<Estudiante> listaEstudiantes, String nombre, int edad,
+            String carrera) {
+        Estudiante nuevoEstudiante = new Estudiante(nombre, edad, carrera);
+        listaEstudiantes.add(nuevoEstudiante);
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        ArrayList<Estudiante> estudiantes = new ArrayList<>();
+        ArrayList<Estudiante> EstudianteList = new ArrayList<>();
 
         System.out.println("------------------------------------------------");
         System.out.println("Bienvenido Al Sistema de Registro de Estudiantes");
@@ -16,6 +23,17 @@ public class main {
         int edad = 0;
         String carrera = "";
 
-        System.out.println("");
+        System.out.println("Ingrese El Nombre Del Estudiante: ");
+        nombre = input.nextLine();
+        System.out.println("Ingrese La Edad Del Estudiante: ");
+        edad = input.nextInt();
+
+        input.nextLine(); // Limpiar el buffer
+
+        System.out.println("Ingrese La Carrera Del Estudiante: ");
+        carrera = input.nextLine();
+
+        agregarEstudiante(EstudianteList, nombre, edad, carrera);
+
     }
 }
