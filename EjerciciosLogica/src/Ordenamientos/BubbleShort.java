a8721028ce7eddb882cf81f98c0dc1796aab9195
zDefcon <antholozadarequelme@gmail.com>
2025-12-04 20:56:48 -0500

    Agregar la clase BubbleShort con un método para ordenar listas de enteros. Se establece la estructura básica para implementar el algoritmo de ordenamiento.

---
+++ EjerciciosLogica/src/Ordenamientos/BubbleShort.java
@@ -0,0 +1,18 @@
+package Ordenamientos;
+
+import java.util.ArrayList;
+
+public class BubbleShort {
+
+    public void ordenarList(ArrayList<Integer> lista) {
+        int n = lista.size();
+        for (int i = 0; i < n-1; i++) { // bucle externo.
+            
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
