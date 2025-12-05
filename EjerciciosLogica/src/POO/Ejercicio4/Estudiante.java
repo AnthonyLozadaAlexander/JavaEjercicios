b7cc904480b27cd4d375a81aa76683aba3af94b2
zDefcon <antholozadarequelme@gmail.com>
2025-12-04 21:26:31 -0500

    Agregar la clase Estudiante con atributos y métodos para gestionar información de estudiantes, incluyendo nombre, edad y carrera.

---
+++ EjerciciosLogica/src/POO/Ejercicio4/Estudiante.java
@@ -0,0 +1,38 @@
+package POO.Ejercicio4;
+
+public class Estudiante {
+    private String nombre;
+    private int edad;
+    private String carrera;
+
+    public Estudiante(String nombre, int edad, String carrera) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.carrera = carrera;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+}
