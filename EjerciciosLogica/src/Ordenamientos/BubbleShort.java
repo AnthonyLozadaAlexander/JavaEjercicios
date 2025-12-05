632abe4bc61e02fcc65ea91be40c42926b225c8f
zDefcon <antholozadarequelme@gmail.com>
2025-12-04 20:59:54 -0500

    Mejorar la legibilidad del método ordenarList en la clase BubbleShort. Se ajusta la indentación y se organiza el código para facilitar su comprensión.

--- EjerciciosLogica/src/Ordenamientos/BubbleShort.java
+++ EjerciciosLogica/src/Ordenamientos/BubbleShort.java
@@ -6,8 +6,14 @@ public class BubbleShort {
 
     public void ordenarList(ArrayList<Integer> lista) {
         int n = lista.size();
-        for (int i = 0; i < n-1; i++) { // bucle externo.
-            
+        for (int i = 0; i < n - 1; i++) { // bucle externo.
+            for (int j = 0; j < n - i - 1; j++) { // Bucle interno
+                if (lista.get(j) > lista.get(j + 1)) {
+                    int temp = lista.get(j);
+                    lista.set(j, lista.get(j + 1));
+                    lista.set(j + 1, temp);
+                }
+            }
         }
     }
 
