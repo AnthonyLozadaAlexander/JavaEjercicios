5682f74e7db501b181ebbe976017749ae99ff897
zDefcon <antholozadarequelme@gmail.com>
2025-12-04 21:54:05 -0500

    Agregar método para registrar estudiantes en la clase main, permitiendo la entrada de múltiples estudiantes y mejorando la gestión de la lista de estudiantes.

--- EjerciciosLogica/src/POO/Ejercicio4/main.java
+++ EjerciciosLogica/src/POO/Ejercicio4/main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
+    static ArrayList<Estudiante> EstudianteList = new ArrayList<>();
 
     public static void agregarEstudiante(ArrayList<Estudiante> listaEstudiantes, String nombre, int edad,
             String carrera) {
@@ -11,9 +12,8 @@ public class main {
         listaEstudiantes.add(nuevoEstudiante);
     }
 
-    public static void main(String[] args) {
+    public static void agregar() {
         Scanner input = new Scanner(System.in);
-        ArrayList<Estudiante> EstudianteList = new ArrayList<>();
 
         System.out.println("------------------------------------------------");
         System.out.println("Bienvenido Al Sistema de Registro de Estudiantes");
@@ -22,18 +22,53 @@ public class main {
         String nombre = "";
         int edad = 0;
         String carrera = "";
+        String next = "";
+        boolean continuar = true;
 
-        System.out.println("Ingrese El Nombre Del Estudiante: ");
-        nombre = input.nextLine();
-        System.out.println("Ingrese La Edad Del Estudiante: ");
-        edad = input.nextInt();
+        do {
+            System.out.println("Ingrese El Nombre Del Estudiante: ");
+            nombre = input.nextLine();
+            System.out.println("Ingrese La Edad Del Estudiante: ");
+            edad = input.nextInt();
 
-        input.nextLine(); // Limpiar el buffer
+            input.nextLine(); // Limpiar el buffer
 
-        System.out.println("Ingrese La Carrera Del Estudiante: ");
-        carrera = input.nextLine();
+            System.out.println("Ingrese La Carrera Del Estudiante: ");
+            carrera = input.nextLine();
 
-        agregarEstudiante(EstudianteList, nombre, edad, carrera);
+            agregarEstudiante(EstudianteList, nombre, edad, carrera);
+            System.out.println("¿Desea Agregar Otro Estudiante?");
+            System.out.println("1 = si / 2 = no");
+            next = input.nextLine();
+            if (next.equals("2")) {
+                continuar = false;
+            } else {
+                continuar = true;
+            }
+        } while (continuar == true);
+
+        System.out.println("------------------------------------------------");
+        System.out.println("           Estudiantes Registrados");
+        System.out.println("------------------------------------------------");
+        for (Estudiante dato : EstudianteList) {
+            System.out.println(dato.getNombre() + "\n"
+                    + dato.getEdad() + "\n"
+                    + dato.getCarrera() + "\n");
+
+        }
+    }
+
+    public void modificar() {
+        System.out.println("Modificar Listas Estudiantes");
+        int index = 0;
+        System.out.println("Ingrese El Indice Del Estudiante A Modificar");
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String opcion = "";
+
+        agregar();
 
     }
 }
