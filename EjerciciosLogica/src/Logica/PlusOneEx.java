464a17d36eb74a87c707836a05586a8adface1f7
zDefcon <antholozadarequelme@gmail.com>
2026-03-11 22:35:13 -0500

    feat: Agregar clase PlusOneEx para incrementar el último dígito de un array de enteros. Se implementa el método plusOne y se incluye un ejemplo en el método main.

---
+++ EjerciciosLogica/src/Logica/PlusOneEx.java
@@ -0,0 +1,15 @@
+package Logica;
+
+public class PlusOneEx {
+    public static void main(String[] args) {
+        int[] result = plusOne(new int[] { 2, 4 }); // Ingresa un array de enteros
+        for (int digit : result)
+            System.out.println(digit + " "); // Imprime el array de enteros
+    }
+
+    public static int[] plusOne(int[] digits) {
+        digits[digits.length - 1]++; // sumamos 1 al ultimo elemento del arreglo
+        return digits;
+
+    }
+}
