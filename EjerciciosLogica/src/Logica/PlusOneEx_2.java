0665b76bca840bfc17556cacab3582fc6ff3b58a
zDefcon <antholozadarequelme@gmail.com>
2026-03-11 23:22:23 -0500

    feat: Modificar el método plusOne en la clase PlusOneEx_2 para implementar la lógica de incremento recursivo y actualizar el ejemplo en el método main.

--- EjerciciosLogica/src/Logica/PlusOneEx_2.java
+++ EjerciciosLogica/src/Logica/PlusOneEx_2.java
@@ -1,16 +1,35 @@
 package Logica;
 
+import java.util.Arrays;
+
 public class PlusOneEx_2 {
     public static void main(String[] args) {
-        int[] result = plusOne(new int[] { 1, 3 }); // Ingresa un array de enteros
+        int[] result = plusOne(new int[] { 1, 9 }); // Ingresa un array de enteros
         for (int digit : result)
             System.out.println(digit + " "); // Imprime el array de enteros
     }
 
     public static int[] plusOne(int[] digits) {
-        digits[digits.length - 1]++; // sumamos 1 al ultimo elemento del arreglo
-        return digits;
+        int[] newDigits;
+        newDigits = plusOne(digits, digits.length - 1);
+        return newDigits;
 
+    }
+
+    public static int[] plusOne(int[] digits, int index) {
+        if (index == -1) {
+            int[] newDigits = new int[digits.length + 1];
+            Arrays.fill(newDigits, 0);
+            newDigits[0] = 1;
+            return newDigits;
+        }
+
+        if (digits[index] == 9) {
+            digits[index] = 0;
+            return plusOne(digits, index - 1);
+        }
+        digits[index]++;
+        return digits;
     }
 
 }
