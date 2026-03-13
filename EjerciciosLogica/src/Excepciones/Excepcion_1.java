2ea5f918c03faf5031ed62c62b88136553712703
zDefcon <antholozadarequelme@gmail.com>
2026-03-12 23:55:15 -0500

    feat: Mejorar el método pedirNumero en la clase Excepcion_1 para manejar excepciones de entrada y proporcionar mensajes de error más claros.

--- EjerciciosLogica/src/Excepciones/Excepcion_1.java
+++ EjerciciosLogica/src/Excepciones/Excepcion_1.java
@@ -6,17 +6,21 @@ import java.util.Scanner;
 public class Excepcion_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int num = pedirNumero(input);
+        System.out.println("El numero guardado es: " + num);
 
     }
 
     public static int pedirNumero(Scanner input) {
         System.out.println("Ingrese El Numero:");
 
-        try{
+        try {
             int num = input.nextInt();
             return num;
-        }catch (InputMismatchException e){
-            System.out.println("Error: ");
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Ingrese un número válido.");
+            input.nextLine(); // Limpiar el buffer de entrada
+            return 0;
         }
     }
 
