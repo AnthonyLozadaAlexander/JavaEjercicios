ac819e6d9fe0c25194b61654059448657b8c1855
zDefcon <antholozadarequelme@gmail.com>
2026-03-15 02:41:37 -0500

    feat: Implementar la lógica para calcular la serie de Fibonacci y solicitar el límite al usuario

--- EjerciciosLogica/src/Logica/SerieFibonacci.java
+++ EjerciciosLogica/src/Logica/SerieFibonacci.java
@@ -1,9 +1,25 @@
 package Logica;
 
+import java.util.Scanner;
+
 public class SerieFibonacci {
     public static void main(String[] args) {
-        int Fo = 0, F1 = 1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bienvenido al programa de la serie de Fibonacci");
+        System.out.println("Ingrese el limite de la serie de Fibonacci: ");
+        int limite = input.nextInt();
 
+    }
+
+    public static void calcularFibonacci(int limite) {
+        int[] serie = new int[limite];
+        serie[0] = 0;
+        serie[1] = 1;
+        for (int i = 0; i < serie.length; i++) {
+            if (i > 1) {
+                serie[i] = serie[i - 1] + serie[i - 2];
+            }
+        }
     }
 
 }
